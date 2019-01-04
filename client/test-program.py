5cf2df55d8d17fc93c16add7837b198ec67eb396
Bartolomé Sintes <bartolome.sintes@gmail.com>
2019-01-04 12:23:09 +0100

    Added parameter in client: file name of the program to be tested

--- client/test-program.py
+++ client/test-program.py
@@ -1,38 +1,46 @@
 # based en https://code-maven.com/mocking-input-and-output-for-python-testing
 
-import requests
-import pytest
-import json
 import importlib
+import json
+import os
+import pytest
+import requests
 
 # Name of the Python program that will be tested
-program_file = "program"
-
-program = importlib.import_module(program_file)
-
-with open("test-values.txt", "r", encoding="utf-8") as file:
+with open("tested-program.txt", "r", encoding="utf-8") as file:
     texto = file.read()
-    texto = texto.replace("'", '"')
-    values = json.loads(texto)
+    if not(os.path.isfile(texto)):
+        print("Error: Program to be tested not found. Please, check file name")
+        exit()
+    else:
+        texto = texto.replace(".py", "")
+        program_file = texto
 
-def test_program():
-    global values
+        program = importlib.import_module(program_file)
 
-    input_values = values["input"]
-    output = []
+        with open("test-values.txt", "r", encoding="utf-8") as file:
+            texto = file.read()
+            texto = texto.replace("'", '"')
+            values = json.loads(texto)
 
-    def mock_input(s):
-        output.append(s)
-        return input_values.pop(0)
+            def test_program():
+                global values
 
-    program.input = mock_input
-    program.print = lambda s : output.append(s)
+                input_values = values["input"]
+                output = []
 
-    program.main()
+                def mock_input(s):
+                    output.append(s)
+                    return input_values.pop(0)
 
-    with open("obtained-result.txt", "w", encoding="utf-8") as file:
-        # saved as json because it is a list
-        json.dump(output, file, ensure_ascii=False)
+                program.input = mock_input
+                program.print = lambda s: output.append(s)
 
-    assert output == values["output"]
+                program.main()
+
+                with open("obtained-result.txt", "w", encoding="utf-8") as file:
+                    # saved as json because it is a list
+                    json.dump(output, file, ensure_ascii=False)
+
+                assert output == values["output"]
 
