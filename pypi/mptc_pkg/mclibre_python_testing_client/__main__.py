a14bb3f96d81a9eb4ef1d28effc58a471cba9e2c
Bartolomé Sintes <bartolome.sintes@gmail.com>
2019-01-04 23:56:27 +0100

    Add PyPI client package

---
+++ pypi/mptc_pkg/mclibre_python_testing_client/__main__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+"""mptc.__main__: executed when mptc directory is called as script."""
+
+
+from .mptc import main
+main()
---
+++ pypi/mptc_pkg/mptc-runner.py
@@ -0,0 +1,8 @@
+"""Convenience wrapper for running mptc directly from source tree."""
+
+
+from mclibre_python_testing_client.mptc import main
+
+
+if __name__ == '__main__':
+    main()
