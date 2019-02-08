f23403a82725713c2e27313e15fdba3842865d99
Bartolomé Sintes <bartolome.sintes@gmail.com>
2019-02-08 20:32:06 +0100

    Allow testing of programas with no output, add tests for four sample programs

--- client/program2.py
+++
@@ -1,8 +0,0 @@
-def main():
-    print("COMPROBADOR DE AÑOS BISIESTOS")
-    fecha = int(input("Escriba un año y le diré si es bisiesto: "))
-
-    print(f"El año {fecha} no es un año bisiesto.")
-
-if __name__ == '__main__':
-    main()
