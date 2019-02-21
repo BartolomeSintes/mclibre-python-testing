40884560020b94259c54df7687ef18399fc425a9
Bartolomé Sintes <bartolome.sintes@gmail.com>
2019-02-21 23:43:31 +0100

    Accept non numeric values in user input

--- server/mpts_1.py
+++ server/mpts_1.py
@@ -4,21 +4,42 @@ NOT_LAST_TEST = True
 LAST_TEST = False
 
 
+def strType(var):
+    try:
+        if int(var) == float(var):
+            return "int"
+    except:
+        try:
+            float(var)
+            return "float"
+        except:
+            return "str"
+
+
 def add_test(input, output, comma):
     print("    {")
     print('       "input" : [', end="")
     if len(input) == 1:
-        print(input[0], end="")
+        if strType(input[0]) != 'str':
+            print(input[0], end="")
+        else:
+            print(f'"{input[0]}"', end="")
     elif len(input) > 1:
         for i in range(len(input) - 1):
-            print(input[i], end="")
+            if strType(input[i]) != 'str':
+                print(input[i], end="")
+            else:
+                print(f'"{input[i]}"', end="")
             print(", ", end="")
-        print(input[-1], end="")
+        if strType(input[-1]) != 'str':
+            print(input[-1], end="")
+        else:
+            print(f'"{input[-1]}"', end="")
     print("],")
     print('       "output" : [', end="")
     for i in range(len(output) - 1):
-        print('"' + output[i] + '", ', end="")
-    print('"' + output[-1] + '"', end="")
+        print(f'"{output[i]}", ', end="")
+    print(f'"{output[-1]}"', end="")
     print("]")
     if comma:
         print("    },")
@@ -157,8 +178,8 @@ def exercise(exercise_id):
             NOT_LAST_TEST,
         )
         # números medianos decimales
-        pies = random.randrange(100, 300) / 10
-        pulgadas = random.randrange(100, 300) / 10
+        pies = float(random.randrange(100, 300) / 10)
+        pulgadas = float(random.randrange(100, 300) / 10)
         cm = (pies * 12 + pulgadas) * 2.54
 
         add_test(
