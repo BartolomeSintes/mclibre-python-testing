021ce27d2211a56c68d200cb6535aa94cc5e6766
Bartolomé Sintes <bartolome.sintes@gmail.com>
2019-02-25 17:11:16 +0100

    Add test for MPTC samples

--- server/mpts_100.py
+++ server/mpts_100.py
@@ -27,40 +27,26 @@ def add_test(input, output, comma):
 
 
 def exercise(exercise_id):
-    if exercise_id == 1000:
-        # Exercise 1000 BEGINNING
-        # http://www.mclibre.org/consultar/python/ejercicios/ej-variables.html
+    if exercise_id == 1001:
+        # Exercise 1001 BEGINNING
+        # http://www.mclibre.org/consultar/python/otros/python-testing-ejemplos.html
 
         # Comprueba salida
-        add_test(
-            [],
-            [
-                "¡Hola, mundo!",
-            ],
-            LAST_TEST,
-        )
-        # Exercise 1000 END
-
-    elif exercise_id == 1001:
-        # Exercise 1000 BEGINNING
-        # http://www.mclibre.org/consultar/python/ejercicios/ej-variables.html
-
-        # Comprueba salida
-        add_test(
-            [],
-            [
-                "SALUDANDO",
-                "¡Hola, mundo!",
-                "¡Adios, amigo!",
-            ],
-            LAST_TEST,
-        )
-
-        # Exercise 1000 END
+        add_test([], ["¡Hola, mundo!"], LAST_TEST)
+        # Exercise 1001 END
 
     elif exercise_id == 1002:
-        # Exercise 1001 BEGINNING
-        # http://www.mclibre.org/consultar/python/ejercicios/ej-variables.html
+        # Exercise 1002 BEGINNING
+        # http://www.mclibre.org/consultar/python/otros/python-testing-ejemplos.html
+
+        # Comprueba salida
+        add_test([], ["SALUDANDO", "¡Hola, mundo!", "¡Adios, amigo!"], LAST_TEST)
+
+        # Exercise 1002 END
+
+    elif exercise_id == 1003:
+        # Exercise 1003 BEGINNING
+        # http://www.mclibre.org/consultar/python/otros/python-testing-ejemplos.html
 
         # número pequeño
         numero = random.randrange(0, 10)
@@ -70,10 +56,11 @@ def exercise(exercise_id):
             [
                 "ESCRIBA NÚMERO",
                 "Escriba un número entero: ",
-                f"Ha escrito el número {numero}",
+                f"Ha escrito el número {numero}.",
             ],
             NOT_LAST_TEST,
         )
+
         # número negativo
         numero = random.randrange(-100, 0)
 
@@ -82,16 +69,17 @@ def exercise(exercise_id):
             [
                 "ESCRIBA NÚMERO",
                 "Escriba un número entero: ",
-                f"Ha escrito el número {numero}",
+                f"Ha escrito el número {numero}.",
             ],
-            LAST_TEST,
+            NOT_LAST_TEST,
         )
 
-    elif exercise_id == 1003:
-        # Exercise 1003 BEGINNING
-        # http://www.mclibre.org/consultar/python/ejercicios/ej-variables.html
+        # Exercise 1003 END
 
-        # número pequeño
+    elif exercise_id == 1004:
+        # Exercise 1004 BEGINNING
+        # http://www.mclibre.org/consultar/python/otros/python-testing-ejemplos.html
+
         a = random.randrange(0, 100) / 10
         b = random.randrange(0, 100) / 10
         c = a + b
@@ -104,5 +92,67 @@ def exercise(exercise_id):
                 "Escriba otro número: ",
                 f"{a} + {b} = {c}",
             ],
+            NOT_LAST_TEST,
+        )
+
+        # Exercise 1004 END
+
+    elif exercise_id == 1005:
+        # Exercise 1005 BEGINNING
+        # http://www.mclibre.org/consultar/python/otros/python-testing-ejemplos.html
+
+        # números crecientes
+        a = random.randrange(0, 1000)
+        b = random.randrange(10 * a, 20000) / 10
+
+        add_test(
+            [a, b],
+            [
+                "NÚMEROS CRECIENTES",
+                "Escriba un número: ",
+                "Escriba otro número más grande: ",
+                "Gracias por su colaboración.",
+            ],
+            NOT_LAST_TEST,
+        )
+
+        # números decrecientes
+        a = random.randrange(100, 1000) / 10
+        b = random.randrange(0, round(a)-1)
+
+        add_test(
+            [a, b],
+            [
+                "NÚMEROS CRECIENTES",
+                "Escriba un número: ",
+                "Escriba otro número más grande: ",
+                "¡Le he pedido un número más grande!",
+            ],
+            NOT_LAST_TEST,
+        )
+
+        # números iguales
+        a = random.randrange(0, 1000)
+        b = a
+
+        add_test(
+            [a, b],
+            [
+                "NÚMEROS CRECIENTES",
+                "Escriba un número: ",
+                "Escriba otro número más grande: ",
+                "¡Le he pedido un número más grande!",
+            ],
             LAST_TEST,
         )
+        # Exercise 1005 END
+
+    elif exercise_id == 1006:
+        # Exercise 1006 BEGINNING
+
+        # número pequeño
+        a = random.randrange(1, 7)
+
+        add_test([a], ["TIRADA DE DADO", f"Ha salido un {a}."], LAST_TEST)
+
+        # Exercise 1006 END
