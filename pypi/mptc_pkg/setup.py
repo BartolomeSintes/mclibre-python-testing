e36a4c3abe1e7106effd2bd31bdb8148318d6d26
Bartolomé Sintes <bartolome.sintes@gmail.com>
2019-04-15 12:30:44 +0200

    Update PyPI client version 0.0.32

--- pypi/mptc_pkg/setup.py
+++ pypi/mptc_pkg/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mclibre_python_testing_client",
-    version="0.0.31",
+    version="0.0.32",
     author="Bartolome Sintes",
     author_email="bartolome.sintes@gmail.com",
     description="Testing tool for some of the exercises in mclibre.org's Python course available at http://www.mclibre.org/consultar/python/",
